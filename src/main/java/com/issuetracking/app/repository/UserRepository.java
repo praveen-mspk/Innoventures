fd349726c03cc6aa56ef9e96590f3e87bb986c4a
theerthagirirvasan <theerthagirivasan@gmail.com>
2026-02-26 21:53:46 +0530

    validation standards done

--- src/main/java/com/issuetracking/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.issuetracking.app.repository;
-
-import com.issuetracking.app.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/com/issuetracking/app/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.issuetracking.app.service;
-
-import com.issuetracking.app.entity.User;
-import java.util.List;
-
-public interface UserService {
-    User createUser(User user);
-    List<User> getAllUsers();
-}
